c50e6c654a888a8292222726aeaf0425cd31aa2a
gusrns83 <gusrns83@gmail.com>
2026-06-01 15:25:42 +0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,17 +1,7 @@
-const CACHE = "ysl-portal-v2";
-
-self.addEventListener("install", e => {
-  self.skipWaiting();
-});
-
+self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
+    caches.keys().then(k => Promise.all(k.map(c => caches.delete(c))))
+    .then(() => self.registration.unregister())
   );
-});
-
-self.addEventListener("fetch", e => {
-  e.respondWith(fetch(e.request));
 });
